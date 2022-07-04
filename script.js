7a55de2c36082e2996568642fed8209884274926
Kleber <kleberst88@gmail.com>
2022-07-04 18:35:21 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -3,24 +3,34 @@ const getPalleteDiv = document.querySelector('#color-palette');
 function generatePalletes() {
   for (let index = 0; index < 4; index += 1) {
     const pallet = document.createElement('div');
+    // Add a classe color aos elementos da palleta de cores
     pallet.className = 'color';
+    // Gera as cores da palleta randomicamente
     pallet.style.backgroundColor = `rgb(${Math.random() * 250},${
       Math.random() * 250
     },${Math.random() * 250})`;
+    // Appenda as palletas de cores ao parent
     getPalleteDiv.appendChild(pallet);
   }
+  // Seta o primeiro elemento com o BG = BLACK
   getPalleteDiv.firstChild.style.backgroundColor = 'black';
+  // Add a classe selected ao primeiro elemento da palleta
+  getPalleteDiv.firstChild.classList.add('selected');
 }
 
 function generatePixels() {
   for (let index = 0; index < 25; index += 1) {
     const getPixelBoard = document.querySelector('#pixel-board');
     const pixel = document.createElement('div');
+    // Add a classe pixel aos pixels gerados
     pixel.className = 'pixel';
+    // Add o BG branco aos pixel
     pixel.style.backgroundColor = 'white';
     getPixelBoard.appendChild(pixel);
   }
 }
+
+function 
 
 window.onload = function () {
   // Gera os 4 primeiros palletes de cores, gerando as cores automaticamente de
