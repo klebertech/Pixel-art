46b5ed78cbd6d2c7296d9826c7cd76a1c011a7aa
Kleber <kleberst88@gmail.com>
2022-07-04 18:07:15 -0300

    Requisitos 4 e 5

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 // Função que gera as paletas de Cores
-
-window.onload = () => {
-  const getPalleteDiv = document.querySelector('#color-palette');
-  function generatePalletes() {
-    for (let index = 0; index < 4; index += 1) {
-      let pallet = document.createElement('div');
-      pallet.className = 'color';
-      pallet.style.backgroundColor = `rgb(${Math.random() * 250},${
-        Math.random() * 250
-      },${Math.random() * 250})`;
-      getPalleteDiv.appendChild(pallet);
-    }
-    getPalleteDiv.firstChild.style.backgroundColor = 'black';
+const getPalleteDiv = document.querySelector('#color-palette');
+function generatePalletes() {
+  for (let index = 0; index < 4; index += 1) {
+    const pallet = document.createElement('div');
+    pallet.className = 'color';
+    pallet.style.backgroundColor = `rgb(${Math.random() * 250},${
+      Math.random() * 250
+    },${Math.random() * 250})`;
+    getPalleteDiv.appendChild(pallet);
   }
+  getPalleteDiv.firstChild.style.backgroundColor = 'black';
+}
+
+function generatePixels() {
+  for (let index = 0; index < 25; index += 1) {
+    const getPixelBoard = document.querySelector('#pixel-board');
+    const pixel = document.createElement('div');
+    pixel.className = 'pixel';
+    pixel.style.backgroundColor = 'white';
+    getPixelBoard.appendChild(pixel);
+  }
+}
+
+window.onload = function () {
+  // Gera os 4 primeiros palletes de cores, gerando as cores automaticamente de
+  // e altera o primeiro elemento para preto.
+
   generatePalletes();
+  generatePixels();
 };
